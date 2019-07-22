23456b05d2208a0f9d62be7f6978eac6fd1e97b2
xuyang-happy <958607524@qq.com>
2019-07-22 21:27:59 +0800

    寻找两个有序数组的中位数

--- src/com/xuyang/leetcode/findMedianSortedArrays/Solution.java
+++ src/com/xuyang/leetcode/findMedianSortedArrays/Solution.java
@@ -1,6 +1,5 @@
 package com.xuyang.leetcode.findMedianSortedArrays;
 
-import java.util.Arrays;
 
 /**
  * @author Mloong
@@ -11,7 +10,7 @@ import java.util.Arrays;
  * <p>
  * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
  * <p>
- * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 可以假设 nums1 和 nums2 不会同时为空。
  */
 public class Solution {
 
@@ -22,35 +21,34 @@ public class Solution {
         新建一个以两个集合长度之和为长度的新数组，从两数组最左边开始比起，把小的放入新集合，并用变量标记后一位置
      */
 
-    public static int[] merge(int[] num1, int[] num2) {
-        //变量用于存储两个集合应该被比较的索引（存入新集合就加一）
-        int a = 0;
-        int b = 0;
-        //定义num3为合并的数组，长度为两者加起来
-        int[] num3 = new int[num1.length + num2.length];
-
-        for (int i = 0; i < num3.length; i++) {
-            if (a < num1.length && b < num2.length) { //两数组都未遍历完，相互比较后加入
-
-                //num1中的数大于num2数字，放入num2的，然后b++
-                if (num1[a] > num2[b]) {
-                    num3[i] = num2[b];
-                    b++;
-                } else {  //num2中的数大于num1数字，放入num1的，然后a++
-                    num3[i] = num1[a];
-                    a++;
-                }
-            } else if (a < num1.length) {   //num2已经遍历完，无需比较，直接将剩余num1加入
-                num3[i] = num1[a];
-                a++;
-            } else if (b < num2.length) {    //num1已经遍历完，无需比较，直接将剩余num2加入
-                num3[i] = num2[b];
-                b++;
-            }
-        }
-        return num3;
-    }
-
+//    public static int[] merge(int[] num1, int[] num2) {
+//        //变量用于存储两个集合应该被比较的索引（存入新集合就加一）
+//        int a = 0;
+//        int b = 0;
+//        //定义num3为合并的数组，长度为两者加起来
+//        int[] num3 = new int[num1.length + num2.length];
+//
+//        for (int i = 0; i < num3.length; i++) {
+//            if (a < num1.length && b < num2.length) { //两数组都未遍历完，相互比较后加入
+//
+//                //num1中的数大于num2数字，放入num2的，然后b++
+//                if (num1[a] > num2[b]) {
+//                    num3[i] = num2[b];
+//                    b++;
+//                } else {  //num2中的数大于num1数字，放入num1的，然后a++
+//                    num3[i] = num1[a];
+//                    a++;
+//                }
+//            } else if (a < num1.length) {   //num2已经遍历完，无需比较，直接将剩余num1加入
+//                num3[i] = num1[a];
+//                a++;
+//            } else if (b < num2.length) {    //num1已经遍历完，无需比较，直接将剩余num2加入
+//                num3[i] = num2[b];
+//                b++;
+//            }
+//        }
+//        return num3;
+//    }
     /*
             对有序数组求中位数
             值得注意的是，int 和double 类型数的转换，直接return的话，是int类型的，除不尽的都舍弃了
@@ -58,17 +56,81 @@ public class Solution {
      */
 
 
-    private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//    private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//
+//        int[] nums = merge(nums1, nums2);
+//
+//        int s = nums.length;
+//        if (s % 2 == 0) {
+//            return (nums[s / 2 - 1] + nums[s / 2]) / 2.0;
+//
+//        } else return nums[(nums.length - 1) / 2] * 1.0;
+//
+//
+//    }
 
-        int[] nums = merge(nums1, nums2);
 
-        int s = nums.length;
-        if (s % 2 == 0) {
-            return (nums[s / 2 - 1] + nums[s / 2]) / 2.0;
+    //思路二： 递归法 官方解法
 
-        } else return nums[(nums.length - 1) / 2] * 1.0;
+    /*
+     在统计中，中位数被用来：
 
+        将一个集合划分为两个长度相等的子集，其中一个子集中的元素总是大于另一个子集中的元素。
 
+    时间复杂度
+    时间复杂度：O(log(min(m,n)))，
+    首先，查找的区间是 [0, m]。 而该区间的长度在每次循环之后都会减少为原来的一半。
+    所以，我们只需要执行 log(m) 次循环。由于我们在每次循环中进行常量次数的操作，
+    所以时间复杂度为 O(log(m))。 由于m≤n，
+    所以时间复杂度是O(log(min(m,n)))。
+
+     */
+    private static double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+
+        if (m > n) { // to ensure m<=n
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {
+                iMin = i + 1; // i is too small
+            } else if (i > iMin && A[i - 1] > B[j]) {
+                iMax = i - 1; // i is too big
+            } else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
     }
 
 
@@ -76,10 +138,8 @@ public class Solution {
         int[] nums1 = new int[]{1, 2, 4, 6, 9};
         int[] nums2 = new int[]{0, 2, 5, 10, 21};
 
-        // System.out.println("排序后:" + Arrays.toString(merge(nums1, nums2)));
-        // System.out.println(merge(nums1, nums2).length);
-        double result = findMedianSortedArrays(nums1, nums2);
-        System.out.println(result);
+        double median = findMedianSortedArrays(nums1, nums2);
+        System.out.println(median);
 
     }
 
