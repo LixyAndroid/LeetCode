7b68fc58888e6f890883e772b1fef2dd9f7ae249
xuyang-happy <958607524@qq.com>
2019-07-25 21:24:04 +0800

    排序算法的复习(上)，选择，插入，冒泡

---
+++ src/com/xuyang/leetcode/sorting/BubbleSort.java
@@ -0,0 +1,66 @@
+package com.xuyang.leetcode.sorting;
+
+/**
+ * @author Li Xuyang
+ * date  2019/7/25 21:02
+ * <p>
+ * 冒泡排序。核心是交换
+ * 最坏情况复杂度和平均情况复杂度均为O(n²)
+ * 最优O(n)
+ */
+public class BubbleSort {
+
+    public static void bubble(int[] values) {
+        boolean swapped;
+        for (int i = 0; i < values.length - 1; i++) {
+
+            swapped = false;
+
+            for (int j = values.length-1; j >i ; j--) {
+
+                //如果后者小于前者，这需要执行交换
+                if (values[j] < values[j-1]){
+
+                    //交换j，和j-1处的值
+                    int temp = values[j];
+                    values[j] =values[j -1];
+                    values[j-1] =temp;
+
+                    //交换完，设置swapped = true;
+
+                    swapped = true;
+                }
+
+            }
+            //swapped 为false的话，就说明。没进入交换，就说明排序完成，直接break;
+            if (!swapped){
+                break;
+            }
+
+        }
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int[] values = {4, 89, 3, 8, 81, 1, 45, 28, 34, 56, 7, 6, 17, 9, 41, 65, 21};
+        System.out.print("排序前: ");
+
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i] + " ");
+        }
+
+        //排序
+        bubble(values);
+
+        System.out.println("");
+        System.out.print("排序后: ");
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i] + " ");
+        }
+
+    }
+}
---
+++ src/com/xuyang/leetcode/sorting/SelectionSort.java
@@ -0,0 +1,66 @@
+package com.xuyang.leetcode.sorting;
+
+/**
+ * @author Li Xuyang
+ * date  2019/7/25 20:13
+ * <p>
+ * 复习排序算法
+ * 选择排序
+ * 选择排序可描述为一种原地比较的算法
+ */
+public class SelectionSort {
+
+    public static void selction(int[] values) {
+
+        //若需要排序的数组长度 <= 1 直接返回，不需要下述判断
+        if (values.length <= 1) {
+            return;
+        }
+
+        int j, minIndex;
+        for (int i = 0; i < values.length - 1; i++) {
+
+            minIndex = i;
+            for (j = i + 1; j < values.length; j++) {
+
+                //如果有更小的值，就把此值的索引j赋值给minIndex
+                if (values[j] < values[minIndex]) {
+
+                    minIndex = j;
+                }
+            }
+
+            int temp = values[minIndex];
+            //最小值索引位置上的数，等于第i个位置上的数 ,此时最小值索引处已经不是最小值，而是原来i处交换过去的
+            //需要在进行一次循环判断，赋值操作
+            values[minIndex] = values[i];
+
+            //交换
+            values[i] = temp;
+
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] values = {4, 89, 3, 8, 81, 1, 45, 28, 34, 56, 7, 6, 17, 9, 41, 65, 21};
+        System.out.print("排序前: ");
+
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i] + " ");
+        }
+
+        //排序
+        selction(values);
+
+        System.out.println("");
+        System.out.print("排序后: ");
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i] + " ");
+        }
+
+    }
+}
