8ac57c4ed8fac9bfff78ff679ec46799f6f257d9
xuyang-happy <958607524@qq.com>
2019-07-27 13:27:28 +0800

    单独定义了树，切把有序的整数数组放到二叉树中

---
+++ src/com/xuyang/leetcode/deleteDuplication/DeleteDuplecate.java
@@ -0,0 +1,95 @@
+package com.xuyang.leetcode.deleteDuplication;
+
+import java.util.HashMap;
+
+/**
+ * @author Li Xuyang
+ * date  2019/7/27 13:17
+ *
+ *
+ * 删除链表重复节点(重复节点只保留一个)
+ */
+public class DeleteDuplecate {
+    /**
+     * HashMap,时间复杂度o(n)
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDulp(ListNode head){
+        if(head==null || head.next==null)
+            return head;
+        HashMap<Integer,Integer> map=new HashMap<Integer, Integer>();
+        ListNode p=new ListNode(-1);//加一个头结点
+        p.next=head;
+        ListNode pre=p;//两个一前一后临时指针
+        ListNode cur=p.next;
+        while(pre!=null && pre.next!=null){
+            if(map.containsKey(cur.val)){
+                pre.next=cur.next;
+                cur=cur.next;
+            }else{
+                map.put(cur.val,1);
+                pre=cur;
+                cur=cur.next;
+            }
+        }
+        return p.next;
+    }
+    /**
+     * 双重循环遍历链表,时间复杂度o(n^2)
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDulp2(ListNode head){
+        if(head==null || head.next==null)
+            return head;
+        ListNode p=head;
+        ListNode root=p;
+        while(p!=null){
+            ListNode q=p;
+            while(q.next!=null){
+                if(p.val==q.next.val){
+                    q.next=q.next.next;
+                }else{
+                    q=q.next;
+                }
+            }
+            p=p.next;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        //-构造单链表start----
+        int[] num = { 2, 3, 3, 5, 7, 8, 8, 8, 9, 9, 10 };
+        ListNode head = new ListNode(num[0]);
+        ListNode pre = head;
+        for (int i = 1; i < num.length; i++) {
+            ListNode node = new ListNode(num[i]);
+            pre.next = node;
+            pre = node;
+        }
+        //-构造单链表end-----
+
+
+
+        //输出
+        System.out.print("删除前：");
+        for (pre = head.next; pre !=null ; pre = pre.next) {
+
+            System.out.print(pre.val + " ");
+        }
+
+        deleteDulp(head);
+        System.out.println();
+        System.out.print("删除后：");
+
+        for (pre = head.next; pre !=null ; pre = pre.next) {
+
+            System.out.print(pre.val + " ");
+        }
+
+
+
+    }
+}
---
+++ src/com/xuyang/leetcode/deleteDuplication/Solution.java
@@ -0,0 +1,81 @@
+package com.xuyang.leetcode.deleteDuplication;
+
+/**
+ * @author Li Xuyang
+ * date  2019/7/27 10:37
+ * 删除链表中重复的节点，不保留 若一个点重复，则全部删除 题目要求
+ */
+public class Solution {
+
+
+    public static ListNode deleteDuplication(ListNode pHead)
+    {
+
+        if (pHead == null){
+            return null;}
+
+        //注意备用头结点，头结点可能被删除
+        ListNode first = new ListNode(-1);
+
+        first.next = pHead;
+        ListNode p = pHead;
+        //前节点
+        ListNode preNode = first;
+
+        while (p != null && p.next != null){
+
+            if (p.val == p.next.val){ //两节点相等
+
+                int val = p.val; //记录val用于判断后面节点是否重复
+                while(p != null && p.val == val){   //这一步用于跳过相等的节点，用于删除
+                    p = p.next;
+                }
+                preNode.next = p; //删除操作，前节点的next直接等于现在的节点，把中间的节点直接跨过
+            }else {
+                preNode = p;
+                p = p.next;
+            }
+        }
+        return first.next;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        //-构造单链表start----
+        int[] num = { 2, 3, 3, 5, 7, 8, 8, 8, 9, 9, 10 };
+        ListNode head = new ListNode(num[0]);
+        ListNode pre = head;
+        for (int i = 1; i < num.length; i++) {
+            ListNode node = new ListNode(num[i]);
+            pre.next = node;
+            pre = node;
+        }
+        //-构造单链表end-----
+
+        System.out.print("删除前：");
+        for (pre = head.next; pre !=null ; pre = pre.next) {
+
+            System.out.print(pre.val + " ");
+        }
+
+        deleteDuplication(head);
+        System.out.println();
+        System.out.print("删除后：");
+
+        for (pre = head.next; pre !=null ; pre = pre.next) {
+
+            System.out.print(pre.val + " ");
+        }
+
+
+    }
+
+
+
+}
