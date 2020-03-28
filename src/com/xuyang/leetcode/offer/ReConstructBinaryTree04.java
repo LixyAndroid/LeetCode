eca9328c9290ee1d822bbcb728d6bf0e550d6668
xuyangli <958607524@qq.com>
2020-03-28 21:45:05 +0800

    重建二叉树

--- src/com/xuyang/leetcode/offer/ReConstructBinaryTree04.java
+++ src/com/xuyang/leetcode/offer/ReConstructBinaryTree04.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
  * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
  * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
  */
-public class reConstructBinaryTree04 {
+public class ReConstructBinaryTree04 {
 
 //    public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
 //        TreeNode root = reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
