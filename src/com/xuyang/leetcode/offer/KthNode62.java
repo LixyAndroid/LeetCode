38f5b7378a34a20e6bf4974896586cb87161e833
xuyangli <958607524@qq.com>
2020-04-14 22:35:52 +0800

    二叉搜索树的第k个结点

--- src/com/xuyang/leetcode/offer/KthNode62.java
+++ src/com/xuyang/leetcode/offer/KthNode62.java
@@ -8,7 +8,7 @@ import java.util.Stack;
  * @date 2020/4/12 14:53
  * 二叉搜索树的第k个结点
  */
-public class KthNode61 {
+public class KthNode62 {
 
     //给定一棵二叉搜索树，请找出其中的第k小的结点。
     // 例如， （5，3，7，2，4，6，8）中，按结点数值大小顺序第三小结点的值为4。
