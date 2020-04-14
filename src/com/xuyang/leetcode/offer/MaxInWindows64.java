a9f30e8da1add6abbc730fb321b35d23eec20e4b
xuyangli <958607524@qq.com>
2020-04-14 22:36:52 +0800

    滑动窗口的最大值

--- src/com/xuyang/leetcode/offer/MaxInWindows64.java
+++ src/com/xuyang/leetcode/offer/MaxInWindows64.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * @date 2020/4/12 15:40
  * 滑动窗口的最大值
  */
-public class MaxInWindows63 {
+public class MaxInWindows64 {
     /*
     给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，他们的最大值分别为{4,4,6,6,6,5}；
      针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个： {[2,3,4],2,6,2,5,1}， {2,[3,4,2],6,2,5,1}， {2,3,[4,2,6],2,5,1}， {2,3,4,[2,6,2],5,1}， {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
@@ -46,7 +46,7 @@ public class MaxInWindows63 {
     public static void main(String[] args) {
         int[] num={2,3,4,2,6,2,5,1};
 
-        MaxInWindows63 maxInWindows63 = new MaxInWindows63();
+        MaxInWindows64 maxInWindows63 = new MaxInWindows64();
 
         ArrayList<Integer> res = maxInWindows63.maxInWindows(num,1);
 
