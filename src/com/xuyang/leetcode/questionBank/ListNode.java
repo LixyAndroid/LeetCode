28ff40c20ca62815b3d70a52b98a6ce78e793c52
xuyangli <958607524@qq.com>
2020-04-19 15:08:43 +0800

    链表

---
+++ src/com/xuyang/leetcode/questionBank/ListNode.java
@@ -0,0 +1,14 @@
+package com.xuyang.leetcode.questionBank;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/4/19 15:07
+ * 链表
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
