041d75e96a7a881338961f838c296b308d0142ff
xuyangli <958607524@qq.com>
2020-04-25 17:36:41 +0800

    完全平方数

---
+++ src/com/xuyang/leetcode/questionBank/NumSquares0279.java
@@ -0,0 +1,51 @@
+package com.xuyang.leetcode.questionBank;
+
+import java.util.Arrays;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/4/25 17:16
+ * 完全平方数
+ */
+public class NumSquares0279 {
+
+    /*
+    动态规划
+     */
+    public int numSquares(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        //bottom case
+        dp[0]=0;
+
+        //pre-calculate the square numbers
+        int max_square_index = (int)Math.sqrt(n)+1;
+        int[] square_nums = new int[max_square_index];
+        for (int i = 0; i < max_square_index; i++) {
+            square_nums[i]=i*i;
+        }
+
+
+        for (int i = 0; i <=n; ++i) {
+            for (int s = 1; s < max_square_index; ++s) {
+                if (i<square_nums[s]){
+                    break;
+
+                }
+
+                dp[i] = Math.min(dp[i],dp[i-square_nums[s]]+1);
+            }
+
+        }
+
+        return dp[n];
+    }
+
+    /*
+
+    时间复杂度：O(n^2/3)，
+    在主步骤中，我们有一个嵌套循环，其中外部循环是 n 次迭代，
+    而内部循环最多需要 n^1/2迭代。
+空间复杂度：O(n)，使用了一个一维数组 dp。
+     */
+}
