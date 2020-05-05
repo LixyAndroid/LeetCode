ff7e7ebb849da8b63e10d402ccbe66e5b05f6edf
xuyangli <958607524@qq.com>
2020-05-05 12:45:57 +0800

    二叉树

---
+++ src/com/xuyang/leetcode/questionBank/TreeNode.java
@@ -0,0 +1,14 @@
+package com.xuyang.leetcode.questionBank;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/5/5 10:56
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
