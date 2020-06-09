69dc61be26dbbc0d91e7cc7e1f67f39cf86b4c45
LixyAndroid <lixy7811@gmail.com>
2020-06-09 22:18:31 +0800

    把数字翻译成字符串

--- src/com/xuyang/leetcode/questionBank/TranslateNum0046.java
+++ src/com/xuyang/leetcode/questionBank/TranslateNum0046.java
@@ -6,7 +6,38 @@ package com.xuyang.leetcode.questionBank;
  * 把数字翻译成字符串
  */
 public class TranslateNum0046 {
-    public int translateNum(int num) {
+
+    /*
+    给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。
+    请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+     */
+    //动态规划，滚动数组优化动态规划
+    public static int translateNum(int num) {
+        String src = String.valueOf(num);
+        int p =0,q=0,r=1;
+        for (int i = 0; i < src.length(); i++) {
+
+            p = q;
+            q = r;
+
+            r=0;
+            r+=q;
+            if (i==0){
+                continue;
+            }
+            String pre = src.substring(i-1,i+1);
+            if (pre.compareTo("25")<=0&& pre.compareTo("10") >= 0){
+                r+=p;
+            }
+        }
+        return r;
 
     }
+
+    public static void main(String[] args) {
+        int num = 12258;
+        int res = translateNum(num);
+        System.out.println(res);
+    }
+
 }
