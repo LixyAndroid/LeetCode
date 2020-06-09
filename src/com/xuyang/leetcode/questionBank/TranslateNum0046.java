9ff3901bb238f86207bb4832de90f7e34b9a2efb
LixyAndroid <lixy7811@gmail.com>
2020-06-09 21:21:15 +0800

    把数字翻译成字符串

---
+++ src/com/xuyang/leetcode/questionBank/TranslateNum0046.java
@@ -0,0 +1,12 @@
+package com.xuyang.leetcode.questionBank;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/6/9 21:20
+ * 把数字翻译成字符串
+ */
+public class TranslateNum0046 {
+    public int translateNum(int num) {
+
+    }
+}
