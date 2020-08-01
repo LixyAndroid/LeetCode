53d4e7b70a4e03a6c4695d46c7a99ead1d7667b8
LixyAndroid <lixy7811@gmail.com>
2020-08-01 23:34:13 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

---
+++ src/com/xuyang/leetcode/offer2/LowestCommonAncestor68.java
@@ -0,0 +1,42 @@
+package com.xuyang.leetcode.offer2;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/1 22:45
+ * 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
+ */
+public class LowestCommonAncestor68 {
+    /*
+    给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+     */
+
+    //递归
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+
+    //迭代
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > q.val) { // 保证 p.val < q.val
+            TreeNode tmp = p;
+            p = q;
+            q = tmp;
+        }
+        while (root != null) {
+            if (root.val < p.val) // p,q 都在 root 的右子树中
+                root = root.right; // 遍历至右子节点
+            else if (root.val > q.val) // p,q 都在 root 的左子树中
+                root = root.left; // 遍历至左子节点
+            else break;
+        }
+        return root;
+    }
+}
