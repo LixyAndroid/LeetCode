f110dfd7cb5ccf2acbcd764033022b1d30ba70e2
LixyAndroid <lixy7811@gmail.com>
2020-08-06 23:14:16 +0800

    二进制求和

--- src/com/xuyang/leetcode/questionBank/AddBinary0067.java
+++ src/com/xuyang/leetcode/questionBank/AddBinary0067.java
@@ -11,10 +11,34 @@ public class AddBinary0067 {
 
     /*
     给你两个二进制字符串，返回它们的和（用二进制表示）。
-
     输入为 非空 字符串且只包含数字 1 和 0。
      */
-    public  String addBinary(String a, String b) {
+
+    //模拟法
+
+    /**
+     * isCarry起关键作用
+     * a字符串 a.charAt(i)==1,isCarry++
+     * b字符串 b.charAt(i)==1,isCarry++
+     * if (isCarry % 2 == 1) { //isCarry 为1 和 3都res.append("1");
+     * res.append("1");
+     * } else {
+     * res.append("0");
+     * }
+     * isCarry为3时候，执行下面的则isCarry =1,下一轮用
+     * isCarry /= 2;
+     * isCarry为1时候，执行下面的则isCarry =0,则不进位
+     * isCarry /= 2;
+     * isCarry为0时候，执行下面的则isCarry =0,则不进位
+     * isCarry /= 2;
+     * isCarry为1时候，执行下面的则isCarry = 2,执行下面的则isCarry =1,下一轮用
+     * isCarry /= 2;
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinary(String a, String b) {
 
         int aLen = a.length() - 1;
         int bLen = b.length() - 1;
@@ -24,30 +48,29 @@ public class AddBinary0067 {
             int aNum = aLen >= 0 ? Integer.parseInt(String.valueOf(a.charAt(aLen))) : 0;
             int bNum = bLen >= 0 ? Integer.parseInt(String.valueOf(b.charAt(bLen))) : 0;
 
-            if (aNum==1){
+            if (aNum == 1) {
                 isCarry++;
             }
-            if (bNum==1){
+            if (bNum == 1) {
                 isCarry++;
             }
 
 
-            if(isCarry%2==1){
+            if (isCarry % 2 == 1) {
                 res.append("1");
-            }else{
+            } else {
                 res.append("0");
             }
-            isCarry/=2;
+            isCarry /= 2;
 
             aLen--;
             bLen--;
 
         }
 
-        if (isCarry==1){
+        if (isCarry == 1) {
             res.append("1");
         }
-
 
         return res.reverse().toString();
     }
@@ -57,42 +80,43 @@ public class AddBinary0067 {
         int n = a.length();
         int m = b.length();
         //保证n是大的
-        if(n<m){
-            return addBinary2(b,a);
+        if (n < m) {
+            return addBinary2(b, a);
         }
-        int length = Math.max(n,m);
 
         StringBuilder sb = new StringBuilder();
 
         int charindex = 0;
         int j = m - 1;
-        for(int i = length-1;i>-1;--i){
+        for (int i = n - 1; i > -1; --i) {
 
-            if(a.charAt(i) == '1'){
+            if (a.charAt(i) == '1') {
                 charindex++;
             }
-            if(j>-1&&b.charAt(j--) == '1'){
+            if (j > -1 && b.charAt(j--) == '1') { //b到头了，则不进来，即拼接a剩余的
                 charindex++;
             }
 
-            if(charindex%2==1){
+            if (charindex % 2 == 1) {
                 sb.append("1");
-            }else{
+            } else {
                 sb.append("0");
             }
-            charindex/=2;
+            charindex /= 2;
 
         }
 
-        if(charindex==1){
+        if (charindex == 1) {
             sb.append("1");
         }
+        return sb.reverse().toString();
+    }
 
-        sb.reverse();
-        return sb.toString();
-
-
-
+    public String addBinary3(String a, String b) {
+        //Integer.toBinaryString 转二进制字符串
+        return Integer.toBinaryString(
+                Integer.parseInt(a, 2) + Integer.parseInt(b, 2)
+        );
     }
 
     public static void main(String[] args) {
