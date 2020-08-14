fba18e4e64b07c50c6256c96b6661cd1b818247c
LixyAndroid <lixy7811@gmail.com>
2020-08-14 23:38:09 +0800

    剑指 Offer 19. 正则表达式匹配

--- src/com/xuyang/leetcode/questionBank/IsMatch0010.java
+++ src/com/xuyang/leetcode/questionBank/IsMatch0010.java
@@ -57,17 +57,17 @@ public class IsMatch0010 {
 
     public boolean isMatch2(String text, String pattern) {
 
-        if (pattern.isEmpty()){
+        if (pattern.isEmpty()) {
             return text.isEmpty();
         }
 
-        boolean first_match = (!text.isEmpty()&&
-                (pattern.charAt(0)==text.charAt(0)||pattern.charAt(0)=='.'));
+        boolean first_match = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
 
-        if (pattern.length()>=2&&pattern.charAt(1)=='*'){
-            return (isMatch(text,pattern.substring(2))||(first_match&&isMatch(text.substring(1), pattern)));
-        }else {
-            return first_match&&isMatch(text.substring(1), pattern.substring(1));
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return (isMatch(text, pattern.substring(2)) || (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
         }
     }
 
