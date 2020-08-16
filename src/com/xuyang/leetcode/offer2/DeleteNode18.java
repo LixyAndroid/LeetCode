4a6b19ec750fb9853782362eeac5094af6de2a0b
LixyAndroid <lixy7811@gmail.com>
2020-08-16 23:59:21 +0800

    剑指 Offer 18. 删除链表的节点

--- src/com/xuyang/leetcode/offer2/DeleteNode18.java
+++ src/com/xuyang/leetcode/offer2/DeleteNode18.java
@@ -46,4 +46,70 @@ public class DeleteNode18 {
         return head;
     }
 
+    public static void deleteNode(ListNode node) {
+        //删除节点了val 为next的val
+        node.val = node.next.val;
+        //删除的下一个，等于下下一个
+        node.next = node.next.next;
+    }
+
+
+    /**
+     * 新增节点
+     *
+     * @param data
+     */
+    public  ListNode insertNode(int data, ListNode head) {
+        ListNode node = new ListNode(data);
+        if (head == null) {
+            head = node;
+            return head;
+        }
+        ListNode curNode = head;
+        //循环找到当前链表的尾节点
+        while (curNode.next != null) {
+            curNode = curNode.next;
+        }
+        //尾节点的指针指向新增加的节点
+        curNode.next = node;
+        return head;
+    }
+
+    /**
+     * 打印链表
+     */
+    public void printList(ListNode head) {
+        ListNode curNode = head;
+        //循环遍历到尾节点
+        while (curNode != null) {
+            System.out.print(curNode.val + "  ");
+            curNode = curNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DeleteNode18 solution = new DeleteNode18();
+
+        //链表的头指针
+        ListNode head = null;
+
+        //新增节点,第一次新增时需要返回头指针，用于定位链表
+        head = solution.insertNode(2, null);
+        solution.insertNode(5, head);
+        solution.insertNode(3, head);
+        solution.insertNode(4, head);
+        solution.insertNode(1, head);
+
+        System.out.println("原链表：");
+        solution.printList(head);
+
+        //head为2,顺序为上述插入的顺序，head.next为5，所以就把5删除了，
+        // 同理传入只head，就把2给删除了,head.next.next就把3给删除了
+        deleteNode(head.next);
+
+        System.out.println("删除后：");
+        solution.printList(head);
+    }
+
 }
