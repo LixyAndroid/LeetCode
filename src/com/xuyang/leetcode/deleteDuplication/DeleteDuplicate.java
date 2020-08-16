9b348ac7c114fb1ffe9bfbff91232920db5d7f79
LixyAndroid <lixy7811@gmail.com>
2020-08-17 00:19:38 +0800

    82. 删除排序链表中的重复元素 II

--- src/com/xuyang/leetcode/deleteDuplication/DeleteDuplicate.java
+++ src/com/xuyang/leetcode/deleteDuplication/DeleteDuplicate.java
@@ -5,61 +5,65 @@ import java.util.HashMap;
 /**
  * @author Li Xuyang
  * date  2019/7/27 13:17
- *
+ * <p>
  * 删除链表重复节点(重复节点只保留一个)
  */
-public class DeleteDuplecate {
+public class DeleteDuplicate {
     /**
      * HashMap,时间复杂度o(n)
+     *
      * @param head
      * @return
      */
-    public static ListNode deleteDulp(ListNode head){
-        if(head==null || head.next==null)
+    public static ListNode deleteDulp(ListNode head) {
+        if (head == null || head.next == null)
             return head;
-        HashMap<Integer,Integer> map=new HashMap<Integer, Integer>();
-        ListNode p=new ListNode(-1);//加一个头结点
-        p.next=head;
-        ListNode pre=p;//两个一前一后临时指针
-        ListNode cur=p.next;
-        while(pre!=null && pre.next!=null){
-            if(map.containsKey(cur.val)){
-                pre.next=cur.next;
-                cur=cur.next;
-            }else{
-                map.put(cur.val,1);
-                pre=cur;
-                cur=cur.next;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ListNode p = new ListNode(-1);//加一个头结点
+        p.next = head;
+        ListNode pre = p;//两个一前一后临时指针
+        ListNode cur = p.next;
+        while (pre != null && pre.next != null) {
+            if (map.containsKey(cur.val)) {
+                pre.next = cur.next;
+                cur = cur.next;
+            } else {
+                map.put(cur.val, 1);
+                pre = cur;
+                cur = cur.next;
             }
         }
         return p.next;
     }
+
     /**
      * 双重循环遍历链表,时间复杂度o(n^2)
+     *
      * @param head
      * @return
      */
-    public static ListNode deleteDulp2(ListNode head){
-        if(head==null || head.next==null)
+    public static ListNode deleteDulp2(ListNode head) {
+        if (head == null || head.next == null)
             return head;
-        ListNode p=head;
-        ListNode root=p;
-        while(p!=null){
-            ListNode q=p;
-            while(q.next!=null){
-                if(p.val==q.next.val){
-                    q.next=q.next.next;
-                }else{
-                    q=q.next;
+        ListNode p = head;
+        ListNode root = p;
+        while (p != null) {
+            ListNode q = p;
+            while (q.next != null) {
+                if (p.val == q.next.val) {
+                    q.next = q.next.next;
+                } else {
+                    q = q.next;
                 }
             }
-            p=p.next;
+            p = p.next;
         }
         return root;
     }
 
     /**
      * 新增节点
+     *
      * @param data
      */
     public ListNode insertNode(int data, ListNode head) {
@@ -92,27 +96,27 @@ public class DeleteDuplecate {
     }
 
     public static void main(String[] args) {
-        DeleteDuplecate deleteDuplecate = new DeleteDuplecate();
+        DeleteDuplicate deleteDuplicate = new DeleteDuplicate();
 
         //链表的头指针
         ListNode head = null;
 
         //新增节点,第一次新增时需要返回头指针，用于定位链表
-        head = deleteDuplecate.insertNode(2, head);
-        deleteDuplecate.insertNode(1, head);
-        deleteDuplecate.insertNode(5, head);
-        deleteDuplecate.insertNode(3, head);
-        deleteDuplecate.insertNode(4, head);
-        deleteDuplecate.insertNode(3, head);
-        deleteDuplecate.insertNode(1, head);
+        head = deleteDuplicate.insertNode(2, head);
+        deleteDuplicate.insertNode(1, head);
+        deleteDuplicate.insertNode(5, head);
+        deleteDuplicate.insertNode(3, head);
+        deleteDuplicate.insertNode(4, head);
+        deleteDuplicate.insertNode(3, head);
+        deleteDuplicate.insertNode(1, head);
 
         System.out.println("原链表：");
-        deleteDuplecate.printList(head);
+        deleteDuplicate.printList(head);
 
         //排序链表
         head = deleteDulp(head);
 
         System.out.println("去重后：");
-        deleteDuplecate.printList(head);
+        deleteDuplicate.printList(head);
     }
 }
--- src/com/xuyang/leetcode/questionBank/DeleteDuplicates0082.java
+++ src/com/xuyang/leetcode/questionBank/DeleteDuplicates0082.java
@@ -1,55 +1,47 @@
-package com.xuyang.leetcode.deleteDuplication;
+package com.xuyang.leetcode.questionBank;
+
 
 /**
  * @author Li Xuyang
- * date  2019/7/27 10:37
- *
- * 删除链表中重复的节点，不保留 若一个点重复，则全部删除 题目要求
- *
+ * @date 2020/8/17 00:08
+ * 删除排序链表中的重复元素 II
  */
-public class Solution {
+public class DeleteDuplicates0082 {
+    //给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+    public static ListNode deleteDuplicates(ListNode head) {
 
-
-    public static void deleteDuplication(ListNode pHead)
-    {
-
-        if (pHead == null){
-            return;}
-
+        if (head == null) {
+            return null;
+        }
         //注意备用头结点，头结点可能被删除
         ListNode first = new ListNode(-1);
 
-        first.next = pHead;
-        ListNode p = pHead;
+        first.next = head;
+        ListNode p = head;
         //前节点
         ListNode preNode = first;
 
-        while (p != null && p.next != null){
+        while (p != null && p.next != null) {
 
-            if (p.val == p.next.val){ //两节点相等
+            if (p.val == p.next.val) { //两节点相等
 
                 int val = p.val; //记录val用于判断后面节点是否重复
-                while(p != null && p.val == val){   //这一步用于跳过相等的节点，用于删除
+                while (p != null && p.val == val) {   //这一步用于跳过相等的节点，用于删除
                     p = p.next;
                 }
                 preNode.next = p; //删除操作，前节点的next直接等于现在的节点，把中间的节点直接跨过
-            }else {
+            } else {
                 preNode = p;
                 p = p.next;
             }
         }
+        return first.next;
     }
-
-
-
-
-
-
 
     public static void main(String[] args) {
 
         //-构造单链表start----
-        int[] num = { 2, 3, 3, 5, 7, 8, 8, 8, 9, 9, 10 };
+        int[] num = {2, 3, 3, 5, 7, 8, 8, 8, 9, 9, 10};
         ListNode head = new ListNode(num[0]);
         ListNode pre = head;
         for (int i = 1; i < num.length; i++) {
@@ -60,23 +52,20 @@ public class Solution {
         //-构造单链表end-----
 
         System.out.print("删除前：");
-        for (pre = head.next; pre !=null ; pre = pre.next) {
+        for (pre = head.next; pre != null; pre = pre.next) {
 
             System.out.print(pre.val + " ");
         }
 
-        deleteDuplication(head);
+        head = deleteDuplicates(head);
         System.out.println();
         System.out.print("删除后：");
 
-        for (pre = head.next; pre !=null ; pre = pre.next) {
+        for (pre = head.next; pre != null; pre = pre.next) {
 
             System.out.print(pre.val + " ");
         }
 
 
     }
-
-
-
 }
