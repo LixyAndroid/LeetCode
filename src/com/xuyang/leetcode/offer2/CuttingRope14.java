998ad7dd9b868faf3d5069994367bd9c6810a99b
LixyAndroid <lixy7811@gmail.com>
2020-08-16 16:19:28 +0800

    剑指 Offer 14- I. 剪绳子

--- src/com/xuyang/leetcode/offer2/CuttingRope14.java
+++ src/com/xuyang/leetcode/offer2/CuttingRope14.java
@@ -30,4 +30,35 @@ public class CuttingRope14 {
         }
         return (int) Math.pow(3, a) * 2;
     }
+
+    /*
+
+     */
+    //体会动态规划
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        int p = 1000000007;
+
+        for (int i = 2; i <= n; i++) {
+            int curMax = 0;
+            for (int j = 1; j < i; j++) {
+                curMax = Math.max(curMax, Math.max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
+
+    //动态规划优化
+    public int integerBreak2(int n) {
+        if (n < 4) {
+            return n - 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(Math.max(2 * (i - 2), 2 * dp[i - 2]), Math.max(3 * (i - 3), 3 * dp[i - 3]));
+        }
+        return dp[n];
+    }
 }
