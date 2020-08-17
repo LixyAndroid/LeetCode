699e6809b72fb4fe99719cef6809eacaeb0756f9
LixyAndroid <lixy7811@gmail.com>
2020-08-17 11:03:34 +0800

    链表

---
+++ src/com/xuyang/leetcode/offer3/ListNode.java
@@ -0,0 +1,14 @@
+package com.xuyang.leetcode.offer3;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/17 10:54
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
