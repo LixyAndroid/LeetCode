5da2488d1d622ed279a452efc56b7e30133b440e
LixyAndroid <lixy7811@gmail.com>
2020-08-18 23:59:09 +0800

    只出现一次的数字

--- src/com/xuyang/leetcode/questionBank/SingleNumber0136.java
+++ src/com/xuyang/leetcode/questionBank/SingleNumber0136.java
@@ -31,9 +31,9 @@ public class SingleNumber0136 {
 
     /*
     (a1⊕a1)⊕(a2⊕a2)⊕⋯⊕(am⊕am)⊕am+1
-​     0⊕0⊕⋯⊕0⊕am+1=am+1
-因此，数组中的全部元素的异或运算结果即为数组中只出现一次的数字。
-​
+     0⊕0⊕⋯⊕0⊕am+1=am+1
+    因此，数组中的全部元素的异或运算结果即为数组中只出现一次的数字。
+
      */
     public int singleNumber(int[] nums) {
         int single=0;
