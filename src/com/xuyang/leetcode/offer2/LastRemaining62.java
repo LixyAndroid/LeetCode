8e644fa6c0aed50951da6edc47b8c44cca36864b
LixyAndroid <lixy7811@gmail.com>
2020-08-19 22:00:29 +0800

    剑指 Offer 62. 圆圈中最后剩下的数字

--- src/com/xuyang/leetcode/offer2/LastRemaining62.java
+++ src/com/xuyang/leetcode/offer2/LastRemaining62.java
@@ -13,7 +13,7 @@ public class LastRemaining62 {
     //
     //例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
 
-   //迭代代替递归
+    //迭代代替递归
     public static int lastRemaining(int n, int m) {
         int f = 0;
         for (int i = 2; i != n + 1; i++) {
@@ -21,10 +21,12 @@ public class LastRemaining62 {
         }
         return f;
     }
+
     //递归
     int lastRemaining2(int n, int m) {
         return f(n, m);
     }
+
     int f(int n, int m) {
         if (n == 1)
             return 0;
@@ -35,7 +37,7 @@ public class LastRemaining62 {
     //模拟，好理解但时间超了
     public static int lastRemaining3(int n, int m) {
 
-        if (n<1||m<1){
+        if (n < 1 || m < 1) {
             return -1;
         }
 
@@ -46,13 +48,13 @@ public class LastRemaining62 {
         }
         int cur = -1;
 
-        while (list.size()>1){
+        while (list.size() > 1) {
 
             for (int i = 0; i < m; i++) {
                 cur++;
 
                 //关键！
-                if (cur==list.size()){
+                if (cur == list.size()) {
                     cur = 0;
                 }
             }
@@ -65,7 +67,6 @@ public class LastRemaining62 {
         return list.get(0);
 
     }
-
 
 
     public static void main(String[] args) {
