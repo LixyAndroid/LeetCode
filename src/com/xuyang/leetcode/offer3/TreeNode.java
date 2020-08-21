98d31bd28b73ad29ffaa7519d0a438d2c1db790d
LixyAndroid <lixy7811@gmail.com>
2020-08-21 18:51:20 +0800

    二叉树

---
+++ src/com/xuyang/leetcode/offer3/TreeNode.java
@@ -0,0 +1,15 @@
+package com.xuyang.leetcode.offer3;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/21 18:46
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
