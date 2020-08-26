190990c2117d7d0cf641cb67ae884ea8dc0d6a24
LixyAndroid <lixy7811@gmail.com>
2020-08-26 23:14:51 +0800

    柱状图中最大的矩形

---
+++ src/com/xuyang/leetcode/questionBank/LargestRectangleArea00842.java
@@ -0,0 +1,38 @@
+package com.xuyang.leetcode.questionBank;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/26 21:56
+ *  84. 柱状图中最大的矩形
+ */
+public class LargestRectangleArea00842 {
+    //给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+    //
+    //求在该柱状图中，能够勾勒出来的矩形的最大面积。
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Stack<Integer> mono_stack = new Stack<>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+
+}
