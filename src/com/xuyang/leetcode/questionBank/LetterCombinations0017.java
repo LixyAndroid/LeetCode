f1f33e7bccd5a5d64335b425ed7e596ff7967981
LixyAndroid <lixy7811@gmail.com>
2020-08-31 18:51:50 +0800

    电话号码的字母组合

---
+++ src/com/xuyang/leetcode/questionBank/LetterCombinations0017.java
@@ -0,0 +1,68 @@
+package com.xuyang.leetcode.questionBank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/31 16:21
+ * 电话号码的字母组合
+ */
+public class LetterCombinations0017 {
+    //给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+    //
+    //给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+    /*
+    首先使用哈希表存储每个数字对应的所有可能的字母，然后进行回溯操作。
+
+回溯过程中维护一个字符串，表示已有的字母排列（如果未遍历完电话号码的所有数字，则已有的字母排列是不完整的）。该字符串初始为空。每次取电话号码的一位数字，从哈希表中获得该数字对应的所有可能的字母，并将其中的一个字母插入到已有的字母排列后面，然后继续处理电话号码的后一位数字，直到处理完电话号码中的所有数字，即得到一个完整的字母排列。然后进行回退操作，遍历其余的字母排列。
+
+回溯算法用于寻找所有的可行解，如果发现一个解不可行，则会舍弃不可行的解。在这道题中，由于每个数字对应的每个字母都可能进入字母组合，因此不存在不可行的解，直接穷举所有的解即可。
+
+     */
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    private void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                //添加完一个数字的某个字母就进行下个数字
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LetterCombinations0017 letterCombinations0017 = new LetterCombinations0017();
+        String str = "2594736";
+        List<String> res = letterCombinations0017.letterCombinations(str);
+        System.out.println(res);
+    }
+}
