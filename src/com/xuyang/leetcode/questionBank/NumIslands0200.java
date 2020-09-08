9f61691f319c92e82db329a1f3fee1b9e14bb47f
LixyAndroid <lixy7811@gmail.com>
2020-09-08 14:33:35 +0800

    岛屿数量

---
+++ src/com/xuyang/leetcode/questionBank/NumIslands0200.java
@@ -0,0 +1,51 @@
+package com.xuyang.leetcode.questionBank;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/9/8 14:26
+ * 岛屿数量
+ */
+public class NumIslands0200 {
+
+    /*
+    给你一个由'1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return num_islands;
+    }
+
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+
+
+    }
+}
