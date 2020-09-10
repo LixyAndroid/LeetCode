99c8297d37983f34a9040eeacb850a5ad0f22b0b
LixyAndroid <lixy7811@gmail.com>
2020-09-10 11:18:15 +0800

    括号生成

---
+++ src/com/xuyang/leetcode/questionBank/GenerateParenthesis0022.java
@@ -0,0 +1,37 @@
+package com.xuyang.leetcode.questionBank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/9/10 10:03
+ * 括号生成
+ */
+public class GenerateParenthesis0022 {
+    //数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
