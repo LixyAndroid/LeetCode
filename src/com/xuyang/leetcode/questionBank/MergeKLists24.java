77a8b9ad8b02b6d5e30115f2b3c07e495e4d8f7c
LixyAndroid <lixy7811@gmail.com>
2020-09-21 19:30:55 +0800

    合并K个升序链表

--- src/com/xuyang/leetcode/questionBank/MergeKLists24.java
+++ src/com/xuyang/leetcode/questionBank/MergeKLists24.java
@@ -39,7 +39,7 @@ public class MergeKLists24 {
         return head.next;
     }
 
-    public ListNode mergeKLists(ArrayList<ListNode> lists) {
+    public ListNode mergeKLists2(ArrayList<ListNode> lists) {
 
         //优先级队列，默认为小根堆
         PriorityQueue<ListNode> pq = new PriorityQueue<>((v1, v2) -> v1.val - v2.val);
@@ -62,6 +62,69 @@ public class MergeKLists24 {
         }
         return dummyHead.next;
 
+    }
+
+    //每次 O(K) 比较 K个指针求 min, 时间复杂度：O(NK)
+    public ListNode mergeKLists3(ListNode[] lists) {
+        int k = lists.length;
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        while (true) {
+            ListNode minNode = null;
+            int minPointer = -1;
+            for (int i = 0; i < k; i++) {
+                if (lists[i] == null) {
+                    continue;
+                }
+                if (minNode == null || lists[i].val < minNode.val) {
+                    minNode = lists[i];
+                    minPointer = i;
+                }
+            }
+            if (minPointer == -1) {
+                break;
+            }
+            tail.next = minNode;
+            tail = tail.next;
+            lists[minPointer] = lists[minPointer].next;
+        }
+        return dummyHead.next;
+    }
+
+
+    public ListNode mergeKLists4(ListNode[] lists) {
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    public ListNode merge(ListNode[] lists, int l, int r) {
+        if (l == r) {
+            return lists[l];
+        }
+        if (l > r) {
+            return null;
+        }
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
     }
 
 }
